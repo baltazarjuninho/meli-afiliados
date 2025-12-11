6b1d6c3ce8571efc74caeea97f6bb4806b9415e1
baltazarjuninho <35103231+baltazarjuninho@users.noreply.github.com>
2025-12-11 20:54:17 -0300

    Delete netlify/functions directory

--- netlify/functions/addProduct.js
+++
@@ -1,8 +0,0 @@
-exports.handler=async(event)=>{
- const fs=require('fs');
- const newProd=JSON.parse(event.body);
- const arr=JSON.parse(fs.readFileSync('products.json','utf8'));
- arr.push(newProd);
- fs.writeFileSync('products.json',JSON.stringify(arr));
- return {statusCode:200,body:'OK'};
-};
--- netlify/functions/getProducts.js
+++
@@ -1,5 +0,0 @@
-exports.handler=async()=>{
- const fs=require('fs');
- const data=fs.readFileSync('products.json','utf8');
- return {statusCode:200, body:data};
-};
